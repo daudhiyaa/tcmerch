a36157ccd012c7f717d53b9fc70ee8b37af79758
borednuna <hanunshaka02@gmail.com>
2022-12-02 01:03:17 +0700

    create responsive navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,29 @@
-import styles from "./Header.module.scss";
+import React, { useState } from "react";
+import "./Header.scss";
 
 function Header() {
+    const [isExpanded, setExpanded] = useState(false);
+
     return(
-        <div></div>
+        <div className="header">
+            <div className="title">Internship</div>
+            <div className={isExpanded ? "expanded" : "navmenu"}>
+                <ul>
+                    <li>Home</li>
+                    <li>Category</li>
+                    <li>New Arrival</li>
+                    <li>Featured Products</li>
+                    <li>Contact</li>
+                </ul>
+                <div className="hamburger"
+                    onClick={() => {
+                        setExpanded(!isExpanded);
+                    }}
+                >
+                    =
+                </div>
+            </div>
+        </div>
     )
 }
 

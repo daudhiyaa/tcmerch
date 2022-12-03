1fccffb8e4db3ec5f87398b532a00e8eda224dcd
Daud Dhiya <daud.dhiya31@gmail.com>
2022-12-04 02:34:59 +0700

    Create FooterCard

--- src/Components/Card/FooterCard/FooterCard.jsx
+++ src/Components/Card/FooterCard/FooterCard.jsx
@@ -1,1 +1,18 @@
 import styles from "./FooterCard.module.scss";
+
+function FooterCard() {
+  return (
+    <div className={styles.footer__card}>
+      <h5 className={styles.footer__cardTitle}>CATEGORIES</h5>
+      <ul>
+        <li>Hoodies</li>
+        <li>Sweatshirts</li>
+        <li>Shirts</li>
+        <li>T-Shirts</li>
+        <li>Jackets</li>
+      </ul>
+    </div>
+  );
+}
+
+export default FooterCard;

0e8688c7a569d2eab640ad583490ea4215a4c39a
borednuna <hanunshaka02@gmail.com>
2022-12-04 23:07:16 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import styles from "./App.module.scss";
-import DivSection from "./Components/DivSection/DivSection";
 
 function App() {
     return(
         <div className={styles.App}>
-            <DivSection/>
         </div>
     )
 }

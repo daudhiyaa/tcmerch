e049b32bdbce930ce8e490b0a675ce085d6bc902
borednuna <hanunshaka02@gmail.com>
2022-12-04 22:59:44 +0700

    Add new font and finalize div secction

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./App.module.scss";
+import DivSection from "./Components/DivSection/DivSection";
 
 function App() {
     return(
-        <div className={styles.App}></div>
+        <div className={styles.App}>
+            <DivSection/>
+        </div>
     )
 }
 

a85c408dd00b4aacd3ffaccacebd3cb1f3fc5501
borednuna <hanunshaka02@gmail.com>
2022-12-06 00:00:02 +0700

    feat: finalize header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "./App.module.scss";
+import Home from "./Components/Sections/Section_Home/Home";
 
 function App() {
-  return <div className={styles.App}></div>;
+  return <div className={styles.App}>
+    <Home/>
+  </div>;
 }
 
 export default App;

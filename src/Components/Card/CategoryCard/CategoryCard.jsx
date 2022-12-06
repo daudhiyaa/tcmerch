7a28743b889bee9b414f69fa9bc388262a9f9d7d
Daud Dhiya <daud.dhiya31@gmail.com>
2022-12-06 23:31:10 +0700

    Update styling for Category Card & Section

--- src/Components/Card/CategoryCard/CategoryCard.jsx
+++ src/Components/Card/CategoryCard/CategoryCard.jsx
@@ -1,9 +1,13 @@
 import styles from "./CategoryCard.module.scss";
+import hoodie from "../../../Assets/whitesweatshirt1.jpg";
 
 function CategoryCard() {
   return (
     <a href="" className={styles.CategoryCard}>
-      <span>SweatShirts</span>
+      <span>
+        {/* <img src={hoodie} alt="" /> */}
+        SweatShirts
+      </span>
     </a>
   );
 }

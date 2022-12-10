a8a30736cc1a83038843a964365ba76d4a618bbc
borednuna <hanunshaka02@gmail.com>
2022-12-10 21:52:56 +0700

    fix: fix footer and home in overall composition

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./App.module.scss";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Sections/Section_Home/Home";
+import Category from "./Components/Sections/CategorySection/CategorySection";
 import DivSection from "./Components/Sections/DivSection/DivSection";
 import Contact from "./Components/Sections/Section_Contact/Contact";
 import Footer from "./Components/Footer/Footer";
@@ -9,11 +10,12 @@ import Footer from "./Components/Footer/Footer";
 function App() {
   return (
     <div className={styles.App}>
-      {/* <Header /> */}
-      {/* <Home /> */}
+      <Header />
+      <Home />
+      <Category />
       <DivSection />
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      <Contact />
+      <Footer />
     </div>
   );
 }

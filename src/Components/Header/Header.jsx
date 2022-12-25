0046691334e3424ca5c1986d985ebdec5ee5d62b
borednuna <hanunshaka02@gmail.com>
2022-12-26 01:26:22 +0700

    feat: (draft) refactor navigation menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,20 +4,20 @@ import "./Header.scss";
 
 function Header() {
   const [isExpanded, setExpanded] = useState(false);
-  const [width, setWidth] = useState(window.visualViewport.width);
+  const [viewportWidth, setWidth] = useState(window.visualViewport.width);
   const [height, setHeight] = useState(window.visualViewport.height);
 
-  window.addEventListener('resize',(e)=>{         
+  window.addEventListener('resize',(e)=>{
     setWidth(e.target.visualViewport.width);
     setHeight(e.target.visualViewport.height);
-    console.log(width);
+    console.log(viewportWidth);
     console.log(height);
   });
 
   return (
     <div className="header">
       <div className="title">TC MERCH</div>
-      <div className={isExpanded ? "expanded" : "navmenu"}>
+      <div className={`navmenu${viewportWidth > 850 ? "" : "shrunk"}`}>
         <ul>
           <li>Home</li>
           <li>Category</li>
@@ -25,14 +25,14 @@ function Header() {
           <li>Featured Products</li>
           <li>Contact</li>
         </ul>
-        <div
+        {/* <div
           className="hamburger"
           onClick={() => {
             setExpanded(!isExpanded);
           }}
         >
           <UilBars />
-        </div>
+        </div> */}
       </div>
     </div>
   );

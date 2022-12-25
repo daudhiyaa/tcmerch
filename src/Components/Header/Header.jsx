f09273a8ceb66710539b2d2752d7797c203dbb70
borednuna <hanunshaka02@gmail.com>
2022-12-26 02:46:38 +0700

    fix: (draft) refactor navmenu classnames

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,14 +10,28 @@ function Header() {
   window.addEventListener('resize',(e)=>{
     setWidth(e.target.visualViewport.width);
     setHeight(e.target.visualViewport.height);
-    console.log(viewportWidth);
-    console.log(height);
+    
+    if (viewportWidth > 850) {
+      setExpanded(false);
+    }
   });
+
+  const getClassnameNavmenu = () => {
+    if (viewportWidth > 850) {
+      return 'navmenu';
+    } else {
+      if (!isExpanded) {
+        return 'navmenuhidden';
+      } else {
+        return 'navmenuExpanded';
+      }
+    }
+  }
 
   return (
     <div className="header">
       <div className="title">TC MERCH</div>
-      <div className={`navmenu${viewportWidth > 850 ? "" : "shrunk"}`}>
+      <div className={getClassnameNavmenu()}>
         <ul>
           <li>Home</li>
           <li>Category</li>
@@ -25,14 +39,14 @@ function Header() {
           <li>Featured Products</li>
           <li>Contact</li>
         </ul>
-        {/* <div
-          className="hamburger"
-          onClick={() => {
-            setExpanded(!isExpanded);
-          }}
-        >
-          <UilBars />
-        </div> */}
+      </div>
+      <div
+      className={`hamburger${viewportWidth > 850 ? "hidden" : "shown"}`}
+      onClick={() => {
+        setExpanded(true);
+      }}
+      >
+        <UilBars />
       </div>
     </div>
   );

abafcff17bed566ba4962d47de73a61a15880f3d
borednuna <hanunshaka02@gmail.com>
2022-12-25 21:57:44 +0700

    feat: detect viewport size in jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,6 +4,15 @@ import "./Header.scss";
 
 function Header() {
   const [isExpanded, setExpanded] = useState(false);
+  const [width, setWidth] = useState(window.visualViewport.width);
+  const [height, setHeight] = useState(window.visualViewport.height);
+
+  window.addEventListener('resize',(e)=>{         
+    setWidth(e.target.visualViewport.width);
+    setHeight(e.target.visualViewport.height);
+    console.log(width);
+    console.log(height);
+  });
 
   return (
     <div className="header">
